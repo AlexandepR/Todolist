c5a8f41642a453fe652a18ec094c9d1026cebcba
Alex <alexandepp@gmail.com>
2021-10-06 12:57:49 +0300

    Updates

---
+++ precache-manifest.a9061b80f1f40ac7d91a09b03c389336.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d848873c54909958b549c1b5b755add",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "be2e916b97719e626f45",
+    "url": "/Todolist/static/css/main.cac71bcf.chunk.css"
+  },
+  {
+    "revision": "fa1ca5fa2f559ffc5f23",
+    "url": "/Todolist/static/js/2.d8d57588.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/Todolist/static/js/2.d8d57588.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be2e916b97719e626f45",
+    "url": "/Todolist/static/js/main.d52c6073.chunk.js"
+  },
+  {
+    "revision": "8c9ba947015c2dd63600",
+    "url": "/Todolist/static/js/runtime-main.e8babc40.js"
+  }
+]);
